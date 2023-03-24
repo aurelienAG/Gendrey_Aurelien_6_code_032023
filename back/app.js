7ae1c99f93975fd080c0138258af7fa360caa74a
Aurélien Gendrey <aurelien@MacBook-Air-de-Aurelien.local>
2023-03-24 02:28:39 +0100

    création du parcours utilisateur

--- back/app.js
+++ back/app.js
@@ -1,12 +1,28 @@
 const express = require('express'); 
+const app = express();
 const mongoose = require('mongoose');
-const app = express(); 
+mongoose.set('strictQuery', true);
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+
 
 mongoose.connect('mongodb+srv://AurelienGendrey:Peanut1989$@cluster0.qamdhy1.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/auth' , userRoutes );
 
 
 module.exports = app; 
